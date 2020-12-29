126218506264cac07fe4bfe462ad93cf40501248
carterStallings <carterstallings1848@gmail.com>
2020-12-28 22:22:54 -0600

    First commit, pushing controller work

---
+++ src/test/java/org/launchcode/hellospringp/HelloSpringPApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.hellospringp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
